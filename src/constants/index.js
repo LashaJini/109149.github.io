2b64ac464b87dc393c16fe98e42792e66d648f16
109149 <109149qwe@gmail.com>
2021-04-08 17:54:28 +0400

    feat: add breakpoints and acdc youtube urls constants

---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+/* breakpoints */
+const bp = {
+  xxs: "0px" /* extra-extra-small */,
+  xs: "320px" /* extra-small */,
+  sm: "600px" /* small */,
+  md: "960px" /* medium */,
+  lg: "1280px" /* large */,
+  xl: "1920px" /* extra-large */,
+};
+
+/* acdc youtube video links */
+const acdcYtUrls = {
+  demonFireURL: "https://www.youtube.com/watch?v=uMh6kz8zN4I",
+  thunderstruckURL: "https://www.youtube.com/watch?v=v2AC41dglnM",
+  hellsBellsURL: "https://www.youtube.com/watch?v=etAIpkdhU9Q",
+  noMansLandURL: "https://www.youtube.com/watch?v=l5MdhcX7Ro0",
+  realizeURL: "https://www.youtube.com/watch?v=ga5qfM2-kog",
+  shotInTheDarkURL: "https://www.youtube.com/watch?v=54LEywabkl4",
+  wildReputationURL: "https://www.youtube.com/watch?v=PZE89NFYb04",
+  backInBlackURL: "https://www.youtube.com/watch?v=pAgnJDJN4VA",
+};
+
+export { bp, acdcYtUrls };
