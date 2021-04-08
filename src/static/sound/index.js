a0190cfc70c2be809b7d2cbdb7a6970d2128020b
109149 <109149qwe@gmail.com>
2021-04-08 17:52:25 +0400

    chore: add .mp3 and .wav files

---
+++ src/static/sound/index.js
@@ -0,0 +1,32 @@
+import thunderstruckWav from "./acdc-thunderstruck.wav";
+import demonFireWav from "./acdc-demon-fire.wav";
+import hellsBellsWav from "./acdc-hells-bells.wav";
+import noMansLandWav from "./acdc-no-mans-land.wav";
+import realizeWav from "./acdc-realize.wav";
+import shotInTheDarkWav from "./acdc-shot-in-the-dark.wav";
+import wildReputationWav from "./acdc-wild-reputation.wav";
+import backInBlackWav from "./acdc-back-in-black.wav";
+
+const acdcWavs = {
+  thunderstruckWav,
+  demonFireWav,
+  hellsBellsWav,
+  noMansLandWav,
+  realizeWav,
+  shotInTheDarkWav,
+  wildReputationWav,
+  backInBlackWav,
+};
+
+export {
+  thunderstruckWav,
+  demonFireWav,
+  hellsBellsWav,
+  noMansLandWav,
+  realizeWav,
+  shotInTheDarkWav,
+  wildReputationWav,
+  backInBlackWav,
+};
+
+export default acdcWavs;
