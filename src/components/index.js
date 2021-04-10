b78b0f2bd58fda2dc32179ecf66a1b5d7014d3ed
109149 <109149qwe@gmail.com>
2021-04-10 14:46:37 +0400

    refactor: re-export components and music files

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,17 @@
+import SoundToggler from "./toggler/sound-toggler/SoundToggler";
+import MusicToggler from "./toggler/music-toggler/MusicToggler";
+import ThemeToggler from "./toggler/theme-toggler/ThemeToggler";
 import App from "./app/App";
 import Divider from "./divider/Divider";
 import Logo from "./logo/Logo";
 import MenuButtons from "./menu/MenuButtons";
 
-export { App, Divider, Logo, MenuButtons };
+export {
+  App,
+  Divider,
+  Logo,
+  MenuButtons,
+  ThemeToggler,
+  SoundToggler,
+  MusicToggler,
+};
---
+++ src/static/music/index.js
@@ -0,0 +1,32 @@
+import noMansLand from "./acdc-no-mans-land.mp3";
+import wildReputation from "./acdc-wild-reputation.mp3";
+import shotInTheDark from "./acdc-shot-in-the-dark.mp3";
+import demonFire from "./acdc-demon-fire.mp3";
+import hellsBells from "./acdc-hells-bells.mp3";
+import realize from "./acdc-realize.mp3";
+import safeInNewYorkCity from "./acdc-safe-in-new-york-city.mp3";
+import thunderstruck from "./acdc-thunderstruck.mp3";
+
+const ACDCMusic = {
+  noMansLand,
+  wildReputation,
+  shotInTheDark,
+  demonFire,
+  hellsBells,
+  realize,
+  safeInNewYorkCity,
+  thunderstruck,
+};
+
+export {
+  noMansLand,
+  wildReputation,
+  shotInTheDark,
+  demonFire,
+  hellsBells,
+  realize,
+  safeInNewYorkCity,
+  thunderstruck,
+};
+
+export default ACDCMusic;
