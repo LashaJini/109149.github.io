c9310f178ea6cb71bc39820cf33102331a5bb69b
109149 <109149qwe@gmail.com>
2021-04-10 18:43:44 +0400

    refactor: toggle between moon and sun svg component

--- src/components/toggler/theme-toggler/ThemeToggler.js
+++ src/components/toggler/theme-toggler/ThemeToggler.js
@@ -1,9 +1,45 @@
 import React from "react";
 import Sun from "./Sun";
 import Moon from "./Moon";
+import { useDarkMode } from "../../../hooks";
+import { css } from "../../../constants";
 
-const ThemeToggler = ({ darkModeIsOne = false }) => {
-  return <>{darkModeIsOne ? <Sun /> : <Moon />}</>;
+// TODO: use context for theme instead of state.
+const ThemeToggler = ({ width, height, fill, darkModeIsOne = false }) => {
+  const [darkModeEnabled, setDarkModeEnabled] = useDarkMode(darkModeIsOne);
+  const [svgFill, setSVGFill] = React.useState(
+    fill || css.vars.textColorPrimary
+  );
+
+  const toggleDarkMode = () => {
+    setDarkModeEnabled((prev) => !prev);
+  };
+
+  const hoverHandler = (color) => {
+    setSVGFill(color);
+  };
+
+  return (
+    <>
+      <div onClick={toggleDarkMode}>
+        {darkModeEnabled ? (
+          <div
+            onMouseEnter={() => hoverHandler(css.theme.light.textColorPrimary)}
+            onMouseLeave={() => hoverHandler(css.theme.dark.textColorPrimary)}
+          >
+            <Sun fill={svgFill} width={width} height={height} />
+          </div>
+        ) : (
+          <div
+            onMouseEnter={() => hoverHandler(css.theme.dark.textColorPrimary)}
+            onMouseLeave={() => hoverHandler(css.theme.light.textColorPrimary)}
+          >
+            <Moon fill={svgFill} width={width} height={height} />
+          </div>
+        )}
+      </div>
+    </>
+  );
 };
 
 export default ThemeToggler;
