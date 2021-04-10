8655ae4e9453709ec4bec08503fca4f3e44be429
109149 <109149qwe@gmail.com>
2021-04-10 18:44:52 +0400

    refactor: re-export hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,11 +1,25 @@
+import useDarkMode from "./use-dark-mode";
+import usePrefersDarkMode from "./use-prefers-dark-mode";
+import useLocalStorage from "./use-local-storage";
+import useEventListener from "./use-event-listener";
 import useHowler from "./use-howler";
 import useACDC from "./use-acdc";
 
 const hooks = {
   useHowler,
   useACDC,
+  usePrefersDarkMode,
+  useLocalStorage,
+  useEventListener,
+  useDarkMode,
 };
 
-export { useHowler, useACDC };
-
 export default hooks;
+export {
+  useHowler,
+  useACDC,
+  usePrefersDarkMode,
+  useLocalStorage,
+  useEventListener,
+  useDarkMode,
+};
