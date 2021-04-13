5084c24ae12992551fea293bc0ea4cf8dc7d5605
109149 <109149qwe@gmail.com>
2021-04-13 11:28:14 +0400

    fix: change fill-rule to fillRule

--- src/components/svg/Stackoverflow.js
+++ src/components/svg/Stackoverflow.js
@@ -14,7 +14,7 @@ const Stackoverflow = ({ width, height, fill }) => {
       >
         <g
           fill="none"
-          fill-rule="evenodd"
+          fillRule="evenodd"
           id="g68"
           transform="translate(0,-0.192)"
         >
@@ -25,7 +25,7 @@ const Stackoverflow = ({ width, height, fill }) => {
           />
           <path
             fill="#f58025"
-            fill-rule="nonzero"
+            fillRule="nonzero"
             d="m 22.987891,76.73 54.025,11.355 2.271,-10.8 -54.025,-11.36 z m 7.149,-25.869 50.046,23.308 4.662,-10.009 -50.047,-23.308 z m 13.848,-24.553 42.428,35.331 7.066,-8.485 -42.428,-35.33 z m 27.387,-26.116 -8.86,6.59 32.948,44.3 8.859999,-6.589 z M 22,102 H 77 V 91 H 22 Z"
             id="path66"
           />
