2b993b3cbeb8939cdccb526013801404467a872c
109149 <109149qwe@gmail.com>
2021-04-14 22:06:34 +0400

    feat: add blockquote component that wraps its children in "quote"

---
+++ src/components/blockquote/Blockquote.js
@@ -0,0 +1,57 @@
+import React from "react";
+import styled from "styled-components";
+import { themes } from "../../constants";
+
+const Blockquote = ({ children, width, left, borderColor }) => {
+  return (
+    <>
+      <Wrapper _width={width} _left={left} _borderColor={borderColor}>
+        <Div _width={width} _left={left} _borderColor={borderColor}>
+          {children}
+        </Div>
+      </Wrapper>
+    </>
+  );
+};
+
+const Wrapper = styled.div`
+  border: 2px solid
+    ${({ _borderColor }) => (_borderColor ? _borderColor : "#e74848")};
+  border-radius: 20px;
+  padding: 7px;
+  width: 100%;
+  height: 100%;
+`;
+
+const Div = styled.div`
+  position: relative;
+
+  &:before {
+    content: "";
+    position: absolute;
+    width: ${({ _width }) => (_width ? _width - 10 : 190)}px;
+    top: -29px;
+    left: ${({ _left }) => (_left ? `${_left}px` : "50%")};
+    border: 6px solid ${themes.vars.bgColorPrimary};
+    transform: translateX(-45%);
+    z-index: 2;
+  }
+
+  &:after {
+    content: "";
+    position: absolute;
+    border: 2px solid
+      ${({ _borderColor }) => (_borderColor ? _borderColor : "#e74848")};
+    border-radius: 0 50px 0 0;
+    width: ${({ _width }) => (_width ? _width : 200)}px;
+    height: 30px;
+    top: -55px;
+    left: ${({ _left }) => (_left ? `${_left}px` : "50%")};
+    transform: translateX(-45%);
+    border-bottom: none;
+    border-left: none;
+    z-index: 3;
+  }
+`;
+
+export default Blockquote;
