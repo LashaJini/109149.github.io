03089868d7566880c6b1a71d981e268687855f64
109149 <109149qwe@gmail.com>
2021-04-20 19:48:14 +0400

    feat: thunder won't escape borders; will have minimum iterations

--- src/components/thunder/Thunder1.js
+++ src/components/thunder/Thunder1.js
@@ -2,13 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 // TODO: do something about the randomness of left/right tree
+// TODO: on hover "play" music
+// TODO: on hover increase shadow
+// TODO: animate
 const Thunder1 = () => {
   const canvasRef = React.useRef();
   const ctxRef = React.useRef();
 
   function resize() {
-    canvasRef.current.width = window.innerWidth;
-    canvasRef.current.height = window.innerHeight;
+    const scale = window.devicePixelRatio || 1;
+
+    canvasRef.current.width = window.innerWidth * scale;
+    canvasRef.current.height = window.innerHeight * scale;
   }
 
   React.useEffect(() => {
@@ -26,26 +31,18 @@ const Thunder1 = () => {
       canvasRef.current.height
     );
 
-    const rootX = canvasRef.current.width / 2;
-    const rootY = canvasRef.current.height - 80;
-    // const INITIAL_HEIGHT = Math.floor(Math.random() * 280 + 300);
-    const INITIAL_HEIGHT = 50;
-    const INITIAL_ANGLE = 0;
-    // const STEM_INITIAL_WIDTH = Math.random() * 120 + 1;
-    const STEM_INITIAL_WIDTH = 10;
-    let STEM_COLOR = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${
-      Math.random() * 255
-    })`;
-    let LEAF_COLOR = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${
-      Math.random() * 255
-    })`;
-    STEM_COLOR = "white";
-    LEAF_COLOR = "white";
-    const INITIAL_SHADOW = 25;
+    const rootX = 160;
+    const rootY = 0;
+    const INITIAL_HEIGHT = 56;
+    const INITIAL_ANGLE = 20;
+    const STEM_INITIAL_WIDTH = 8;
+    const STEM_COLOR = "white";
+    const LEAF_COLOR = "white";
+    const INITIAL_SHADOW = 30;
     const SHADOW_COLOR = "cyan";
-    const LEAF_THRESHOLD = 38; // as big as possible to decrease render time
-    const STEM_WIDTH_DECLINE_FACTOR = 0.78;
-    const TREE_HEIGHT_DECLINE_FACTOR = 0.968; // 1 - no decline, 0 - full decline
+    const LEAF_THRESHOLD = 30; // as big as possible to decrease render time
+    const STEM_WIDTH_DECLINE_FACTOR = 0.8;
+    const TREE_HEIGHT_DECLINE_FACTOR = 0.958; // 1 - no decline, 0 - full decline
 
     ctxRef.current.strokeStyle = STEM_COLOR;
     ctxRef.current.fillStyle = LEAF_COLOR;
@@ -53,52 +50,116 @@ const Thunder1 = () => {
     ctxRef.current.shadowBlur = INITIAL_SHADOW;
     ctxRef.current.lineCap = "round";
 
-    function drawTree(rootX, rootY, height, angle, stemWidth) {
-      drawStem(rootX, rootY, stemWidth, height, angle);
+    let iter = 15;
+    function drawTree(
+      rootX,
+      rootY,
+      height,
+      angle,
+      stemWidth,
+      last,
+      drawProbability = 0.18
+    ) {
+      const { x: newX, y: newY, angle: lastAngle } = drawStem(
+        rootX,
+        rootY,
+        stemWidth,
+        height,
+        angle,
+        last
+      );
 
       if (height < LEAF_THRESHOLD) {
-        drawLeaf(height);
-        ctxRef.current.restore(); // restore saved canvas state; pops the stack; does nothing if nothing was saved before
         return;
       }
-      const dTheta = Math.random() > 0.5 ? Math.random() * 24 + 2 : 0;
+      const dTheta = Math.random() > 0.5 ? Math.random() * 20 + 2 : 0;
       const stemNewWidth = stemWidth * STEM_WIDTH_DECLINE_FACTOR;
       const newHeight = height * TREE_HEIGHT_DECLINE_FACTOR;
-      Math.random() > 0.2 &&
-        drawTree(0, -height, newHeight, angle + dTheta, stemNewWidth);
-      Math.random() > 0.2 &&
-        drawTree(0, -height, newHeight, angle - dTheta, stemNewWidth);
+
+      if (iter > 0) {
+        drawTree(
+          0,
+          height,
+          newHeight,
+          angle + dTheta,
+          stemNewWidth,
+          { x: newX, y: newY, angle: lastAngle },
+          drawProbability * 1.1
+        );
+        drawTree(
+          0,
+          height,
+          newHeight,
+          angle - dTheta,
+          stemNewWidth,
+          { x: newX, y: newY, angle: lastAngle },
+          drawProbability * 1.1
+        );
+        iter--;
+      } else {
+        Math.random() > drawProbability &&
+          drawTree(
+            0,
+            height,
+            newHeight,
+            angle + dTheta,
+            stemNewWidth,
+            { x: newX, y: newY, angle: lastAngle },
+            drawProbability * 1.1
+          );
+        Math.random() > drawProbability &&
+          drawTree(
+            0,
+            height,
+            newHeight,
+            angle - dTheta,
+            stemNewWidth,
+            { x: newX, y: newY, angle: lastAngle },
+            drawProbability * 1.1
+          );
+      }
 
       ctxRef.current.restore();
     }
 
-    function drawStem(rootX, rootY, stemWidth, height, angle) {
-      ctxRef.current.beginPath();
-      ctxRef.current.save(); // save canvas' current state & push to stack
-
-      // ctxRef.current.lineCap = Math.random() > 0.5 ? "round" : "square";
-      // ctxRef.current.shadowBlur =
-      //   Math.random() > 0.5
-      //     ? INITIAL_SHADOW
-      //     : Math.floor(INITIAL_SHADOW * Math.random()) + 5;
-      ctxRef.current.lineWidth = stemWidth;
-      ctxRef.current.translate(rootX, rootY - 2);
+    // `last.angle -` kinda makes it look more beautiful
+    function drawStem(rootX, rootY, stemWidth, height, angle, last) {
       let rotationAngle = (angle * Math.PI) / 180;
-      rotationAngle = Math.random() > 0.5 ? rotationAngle : -rotationAngle;
-      ctxRef.current.rotate(rotationAngle);
+      rotationAngle =
+        last.angle - Math.random() > 0.5 ? rotationAngle : -rotationAngle;
 
-      ctxRef.current.moveTo(0, 0);
-      ctxRef.current.lineTo(0, -height); // upwards
-      ctxRef.current.stroke(); // its like border
+      let newX = last.x;
+      newX -= height * Math.sin(rotationAngle);
+      let newY = last.y;
+      newY += height * Math.cos(rotationAngle);
+      let newAngle = rotationAngle;
+
+      if (
+        newX > canvasRef.current.innerWidth ||
+        newX < 0 ||
+        newY > canvasRef.current.innerHeight ||
+        newY < 0
+      ) {
+        return last;
+      }
+
+      ctxRef.current.beginPath();
+      ctxRef.current.save();
+      ctxRef.current.lineWidth = stemWidth;
+      ctxRef.current.moveTo(last.x, last.y);
+      ctxRef.current.lineTo(newX, newY);
+      ctxRef.current.stroke();
+
+      let result = { x: newX, y: newY, angle: newAngle };
+      console.log(result);
+      return result;
     }
 
-    function drawLeaf(height) {
-      // ctxRef.current.beginPath();
-      // ctxRef.current.arc(0, -height, 20, 0, Math.PI / 2);
-      // ctxRef.current.fill();
-    }
-
-    drawTree(rootX, rootY, INITIAL_HEIGHT, INITIAL_ANGLE, STEM_INITIAL_WIDTH);
+    drawTree(rootX, rootY, INITIAL_HEIGHT, INITIAL_ANGLE, STEM_INITIAL_WIDTH, {
+      x: rootX,
+      y: rootY,
+      angle: INITIAL_ANGLE,
+    });
   }
 
   React.useEffect(() => {
