1aafaddb4a83870523c386fb02ba91b7477af93e
109149 <109149qwe@gmail.com>
2021-04-26 12:51:52 +0400

    feat: add new font family

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -1,9 +1,10 @@
 import React from "react";
+import styled from "styled-components";
 import { Fadeable, Jiggle } from "../";
 
 const Title = ({ children, delay = 55 }) => {
   return (
-    <h2>
+    <H2>
       {typeof children === "string" &&
         children.split("").map((c, i) => {
           if (c === " ") return <span key={i}> </span>;
@@ -14,8 +15,14 @@ const Title = ({ children, delay = 55 }) => {
             </Fadeable>
           );
         })}
-    </h2>
+    </H2>
   );
 };
+
+const H2 = styled.h2`
+  font-family: "Orelega One";
+  font-size: 1.8rem;
+  letter-spacing: 4px;
+`;
 
 export default Title;
