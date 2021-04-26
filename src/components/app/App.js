318597d9233406f418c90208323ad32d488e4d21
109149 <109149qwe@gmail.com>
2021-04-26 19:55:47 +0400

    Replace BrowserRouter with HashRouter

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import { FoF, Main } from "../";
 
 const App = () => {
   // console.log("render");
   return (
-    <Router>
+    <Router basename="/">
       <Switch>
         <Route exact path="/">
           <Main />
